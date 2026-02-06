3ae168da83bd5aca69543023ddac2092d2f7e5fd
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2026-02-06 12:21:59 +0100

    New select between ESP32 and ESP8266

--- include/Network.h
+++ include/Network.h
@@ -1,11 +1,16 @@
 #pragma once
 
-#ifdef CONFIG_IDF_TARGET_ESP32
+#if defined(ESP32) || defined(LIBRETINY)
+// #ifdef CONFIG_IDF_TARGET_ESP32
+    #include <AsyncTCP.h>
     #include <WiFi.h>
-#else
+#elif defined(ESP8266)
     #include <ESP8266WiFi.h>
+    #include <ESPAsyncTCP.h>
     #include <ESP8266mDNS.h>
+#elif defined(TARGET_RP2040) || defined(TARGET_RP2350) || defined(PICO_RP2040) || defined(PICO_RP2350)
+    #include <RPAsyncTCP.h>
+    #include <WiFi.h>
 #endif
 
-#include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
