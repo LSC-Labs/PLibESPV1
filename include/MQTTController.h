356d93005c18ccb4e39700c2edd7516f83096f14
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2026-02-06 12:22:29 +0100

    queue inserted and dispatch function

--- include/MQTTController.h
+++ include/MQTTController.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <Arduino.h>
 #include <AsyncMqttClient.h>
-#include <ModuleInterface.h>
+#include <ApplModule.h>
+#include <queue>
 
 
 struct MQTTConfig {
@@ -26,25 +27,51 @@ struct MQTTStatus {
     String                           DisConReasonString;
 };
 
+class MQTTMessage {
+    public:
 
-class CMQTTController : public AsyncMqttClient, public IModule {
+    /**
+     * The received message from message broker..
+     */
+    char * Message;
+
+    MQTTMessage(const char *pszMessage) {
+        Message = strdup(pszMessage);
+    }
+
+    ~MQTTMessage() {
+        if(Message) {
+            free(Message);
+        }
+    }
+};
+/**
+ * Message Broker as a module
+ */
+class CMQTTController : public AsyncMqttClient, public ApplModule {
     private:
+        bool   m_bRealtimeMode = false;
         size_t m_nMessageBufferSize = 0;
         char * m_pszMessageBuffer = nullptr;
+        char * m_pszLastWillTopic = nullptr;   // see last will - has to stay valid as long as mqtt client exists
+        char * m_pszLastWillPayload = nullptr; // see Last will - has to stay valid as long as mqtt client exists
         unsigned long m_ulLastHeartBeat = 0;
+        std::queue<MQTTMessage * > m_tMessageQeue; // FiFo queue with Message pointer...
 
     public:
         MQTTConfig Config;
         MQTTStatus Status;
 
         CMQTTController();
+        ~CMQTTController();
+        
         void setup();
 
         void readConfigFrom(JsonObject &oCfg) override;
         void writeConfigTo(JsonObject &oCfg, bool bHideCritical) override;
         void writeStatusTo(JsonObject &oCfg) override;
         int receiveEvent(const void * pSender, int nMsg, const void * pMessage, int nClass) override;
-
+        void dispatch() override;
 
         bool isSessionActiv();
         void publishEvent(String strTopic, JsonDocument &oDataDoc,   bool bToHA = false);
