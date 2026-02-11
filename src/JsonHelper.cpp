9e7542b01c5dc3be97bcb44af98878c3d7fea65b
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2026-02-11 11:39:38 +0100

    payload without debug messages

--- src/JsonHelper.cpp
+++ src/JsonHelper.cpp
@@ -7,8 +7,6 @@
 namespace LSC {
 
     JsonObject createPayloadStructure(const char* pszCommand, const char *pszDataType, JsonDocument &oPayloadDoc, const char *pszData) {
-        DEBUG_FUNC_START_PARMS("%s,%s,...",NULL_POINTER_STRING(pszCommand),NULL_POINTER_STRING(pszDataType));
-
         char pszBuffer[256];
         sprintf(pszBuffer,"{\"command\":\"%s\",\"data\":\"%s\"",pszCommand,pszDataType);
         String strData = pszBuffer;
@@ -17,13 +15,10 @@ namespace LSC {
             strData += pszData;
         }
         strData += "}";
-        DEBUG_INFO(" --> creating payload:");
-        DEBUG_INFOS("%s",strData.c_str());
         DeserializationError error = deserializeJson(oPayloadDoc,strData);
         if(error) {
             ApplLogErrorWithParms("WS: Error creating payload structure : %s",error.c_str());
         } 
-        DEBUG_FUNC_END();
         return(pszData == nullptr ? CreateJsonObject(oPayloadDoc,"payload") : oPayloadDoc["payload"]);
     }
 
