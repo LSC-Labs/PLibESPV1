7f1e66b0570c292026df462137d420f0642fd129
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2026-02-22 23:45:52 +0100

    Minor fixes after testruns

--- src/Security.cpp
+++ src/Security.cpp
@@ -1,7 +1,7 @@
 #ifndef DEBUG_LSC_SECURITY
     #undef DEBUGINFOS
 #endif
- 
+  
 
 // Using library : https://github.com/me-cooper/esp8266-aes-128-cbc
 #include <LSCUtils.h>
@@ -17,15 +17,15 @@
 
 // Ensure the APPL_SECUIRTY_TOKEN_PASS is exact 16 chars long
 #ifndef APPL_SECURITY_TOKEN_PASS
-    #define APPL_SECUIRTY_TOKEN_PASS "Kis8%$vvQ@ä+qw12"
+    #define APPL_SECURITY_TOKEN_PASS "Kis8%$vvQ@ä+qw12"
 #endif
 
-// Key - embeded into the token
+// Unique Key - embeded into the security token
 #ifndef APPL_SECURITY_TOKEN_KEY
     #define APPL_SECURITY_TOKEN_KEY "ds$woEir=ncn<jek"
 #endif
 
-const String strAppPass = APPL_SECUIRTY_TOKEN_PASS;  // exact 16 chars !
+const String strAppPass = APPL_SECURITY_TOKEN_PASS;  // exact 16 chars !
 
 #pragma region Base64 encoding/decoding
 
@@ -82,11 +82,20 @@ String getBase64EncodedString(const char *tArray, int nSizeOfArray) {
 }
 #pragma endregion
 
+
+
+void convertStringToArray(const char * psz, uint8_t * tArray, size_t nArraySize) {
+    memset(tArray,'\0',nArraySize);
+    for (size_t i = 0; i < nArraySize; ++i) {
+        if(psz && * psz) tArray[i] = *psz++;
+    }
+}
 /** Convert a string with hex data into an array */
-void convertStringToHex(const String &str, uint8_t *hexArray, size_t arraySize) {
-    memset(hexArray,'\0',arraySize);
-    for (size_t i = 0; i < arraySize; ++i) {
-        if(str.length() > i) hexArray[i] = str[i];
+void convertStringToArray(const String & str, uint8_t *tArray, size_t nArraySize) {
+    convertStringToArray(str.c_str(),tArray,nArraySize);
+    memset(tArray,'\0',nArraySize);
+    for (size_t i = 0; i < nArraySize; ++i) {
+        if(str.length() > i) tArray[i] = str[i];
     }
 }
 
@@ -126,14 +135,24 @@ const String getRandomNumbers(byte tBuffer[], int nBufferLen) {
   
 #pragma region PKCS7 Padding 
   
-void insertPkcs7Padding(byte* pData, size_t nDataLength, size_t nBlockSize) {
+/**
+ * Insert the PKCS7 Padding 
+ * - to fill up data until the block end
+ * Ensure, enough memory is allocated (!)
+ * -> if the datalength hits exact the boundary of the block, an extra block is needed (!)
+ * Best is always to allocate one block extra to ensure (!)
+ * @param pData Pointer to the data
+ * @param nDataLength Length of the data.
+ * @param nBlockSize Size of a block (default = 16)
+ */
+void insertPkcs7Padding(byte* pData, size_t nDataLength, size_t nBlockSize = 16) {
     size_t nPaddingLen = nBlockSize - (nDataLength % nBlockSize);
     for (size_t i = nDataLength; i < nDataLength + nPaddingLen; i++) {
         pData[i] = nPaddingLen;
     }
 }
   
-void removePkcs7Padding(byte* pData, size_t& nDataLength, size_t nBlockSize) {
+void removePkcs7Padding(byte* pData, size_t& nDataLength, size_t nBlockSize = 16) {
     size_t nPaddingLen = pData[nDataLength - 1];
     if (nPaddingLen <= nBlockSize) {
         nDataLength -= nPaddingLen;
@@ -144,43 +163,121 @@ void removePkcs7Padding(byte* pData, size_t& nDataLength, size_t nBlockSize) {
   
 #pragma region AES encryption / decryption
 
-
-String encryptDataToBase64(String strData, byte tIV[]) {
-    // Load into tData and fill with paddings.
-    int nLen = strData.length();
-    int nBlocks = nLen / 16 + 1;
-    // uint8_t nPadding = nBlocks * 16 - nLen;
-    uint8_t tData[nBlocks * 16];
-    memcpy(tData, strData.c_str(), nLen);
-    insertPkcs7Padding(tData, nLen, 16);
-
-    uint8_t aes_key[16];
-    convertStringToHex(strAppPass,aes_key,sizeof(aes_key));
-
-    br_aes_big_cbcenc_keys encCtx;
-    br_aes_big_cbcenc_init(&encCtx, aes_key, 16);
-    br_aes_big_cbcenc_run(&encCtx, tIV, tData, nBlocks * 16);
-
-    String strResult = getBase64EncodedString((const char*) tData,sizeof(tData));
-    return strResult;
-
+void writeArrayToSerial(byte tData[], int nLen) {
+    for(int nIdx = 0; nIdx < nLen; nIdx++ ) Serial.printf("0x%02X ",tData[nIdx]);
+    Serial.println(";");
 }
 
-String decryptDataFromBase64(String strData, byte tIV[]) {
-    DEBUG_FUNC_START_PARMS("%s",strData.c_str());
+
+/**
+ * @brief encrypts the data 
+ * As the IV is set to the last block, cover it to avoid destroying the users IV.
+ * Ensure, pData has enough buffer allocated (!) (datalen/16 + 1) * 16;
+ * @param pData Data to be encrypted (Buffer has to be allocated to align to 16 bytes block size)
+ * @param nDataLen Length of data to be encrypted
+ * @param tIV the initial vector to be used (16 bytes expected)
+ * @param pKeyPhrase Pointer to the Keyphrase to be used 
+ * @param nKeyLen Length of the keyphrase (has to be aligned to 16 bytes)
+ */
+int encryptData(void * pData, size_t nDataLen, byte tIV[], void * pKeyPhrase, size_t nKeyLen) {
+
+    // safe the IV to be used without impacting the users one...
+    byte _IV[16] = {0};
+    memcpy(_IV,tIV,16);
+
+    insertPkcs7Padding((byte *) pData,nDataLen,16);
+
+    int nNumBlocks = (nDataLen / 16) + 1;
+
+    br_aes_big_cbcenc_keys encCtx;
+    br_aes_big_cbcenc_init(&encCtx, pKeyPhrase, nKeyLen);
+    br_aes_big_cbcenc_run(&encCtx, _IV, pData, nNumBlocks * 16);
+    return(nNumBlocks * 16);
+}
+
+/**
+ * @brief encrypt the data by using the keyPhrase.
+ */
+void encryptData(void * pData, size_t nDataLen, byte tIV[], const char * pszKeyPhrase ) {
+    // Prepare the KeyPhrase to be aligned to 16 bytes
+    byte tKeyPhrase[16] = {0};
+    convertStringToArray(pszKeyPhrase,tKeyPhrase, sizeof(tKeyPhrase));
+    encryptData(pData,nDataLen, tIV, tKeyPhrase, sizeof(tKeyPhrase));
+}
+
+/**
+ * encrypt the data to a base 64 string.
+ * Using a cbc algo with 16 byte of block size.
+ * @param strData String with the data to be encoded
+ * @param tIV[] the initial vector (16 bytes)
+ */
+String encryptDataToBase64(String strData, byte tIV[]) {
+    // Prepare the numbers.
+    // - Data len
+    // - needed blocks (i.W. 15/16 == 0 => allocate always one for padding)
+    int nBlocks = (strData.length() / 16) + 1;
+    byte tData[nBlocks * 16];
+    memcpy(tData, strData.c_str(), strData.length());
+
+    encryptData(tData,strData.length(),tIV,strAppPass.c_str());
+    String strResult = getBase64EncodedString((const char*) tData,sizeof(tData));
+    return strResult;
+}
+
+
+/**
+ * @brief decrypts the data 
+ * As the IV is set to the last block, it will be covered to avoid destroying the users IV.
+ * Ensure, pData has enough buffer allocated (!) (datalen/16 + 1) * 16;
+ * @param pData Data to be encrypted (Buffer has to be allocated to align to 16 bytes block size)
+ * @param nDataLen Length of encrypted data (is aligned to 16 byte block size)
+ * @param tIV the initial vector to be used (16 bytes expected)
+ * @param pKeyPhrase Pointer to the Keyphrase to be used 
+ * @param nKeyLen Length of the keyphrase (has to be aligned to 16 bytes)
+ */
+void decryptData(void * pData, size_t & nDataLen, const byte tIV[], void * pKeyPhrase, size_t nKeyLen) {
+   
+    // copy the IV to protect the users one...
+    byte _IV[16] = {0};
+    memcpy(_IV,tIV,16);
+    br_aes_big_cbcdec_keys decCtx;
+    br_aes_big_cbcdec_init(&decCtx, pKeyPhrase, nKeyLen);
+    br_aes_big_cbcdec_run(&decCtx, _IV, pData, nDataLen);
+
+    // Remove the padding and set the datalen to the correct one.
+    removePkcs7Padding((byte *) pData,nDataLen,16);
+}
+
+/**
+ * @brief decrypt the data by using the keyPhrase as string pointer.
+ * @param pData Data to be encrypted (Buffer has to be allocated to align to 16 bytes block size)
+ * @param nDataLen Length of encrypted data (is aligned to 16 byte block size) and will be set
+ *                 to the decrypted data size after decryption.
+ * @param tIV the initial vector to be used (16 bytes expected) - will stay untouched
+ * @param pKeyPhrase Pointer to the Keyphrase to be used 
+ * @param nKeyLen Length of the keyphrase (has to be aligned to 16 bytes)
+ */
+void decryptData(void * pData, size_t & nDataLen, const byte tIV[], const char * pszKeyPhrase ) {
+
+    // Prepare the KeyPhrase to be aligned to 16 bytes
+    // and decrpyt the data
+    byte tKeyPhrase[16] = {0};
+    convertStringToArray(pszKeyPhrase,tKeyPhrase, sizeof(tKeyPhrase));
+    decryptData(pData,nDataLen, tIV, tKeyPhrase, sizeof(tKeyPhrase));
+}
+
+/**
+ * @brief Decrypt the base 64 string, is using the application key to decrypt
+ * @param strData the base64 encoded string
+ * @param the IV - will stay untouched
+ */
+String decryptDataFromBase64(String strData, const byte tIV[]) {
     int nInputLen = strData.length();
     char *pszEncodedData = const_cast<char *>(strData.c_str());
-    uint8_t tData[nInputLen];   // Larger then needed...
-    int nRealDataLen = base64_decode_chars(pszEncodedData,nInputLen,(char *)tData);
-    uint8_t aes_key[16];
-    convertStringToHex(strAppPass,aes_key,sizeof(aes_key));
-    int nBlocks = nRealDataLen / 16;
-    br_aes_big_cbcdec_keys decCtx;
-    br_aes_big_cbcdec_init(&decCtx, aes_key, 16);
-    br_aes_big_cbcdec_run(&decCtx, tIV, tData, nBlocks * 16);
-
+    uint8_t tData[nInputLen];   // Is larger then needed...
+    size_t nRealDataLen = base64_decode_chars(pszEncodedData,nInputLen,(char *)tData);
     size_t nDataLen = nRealDataLen;
-    removePkcs7Padding(tData, nDataLen, 16);
+    decryptData(tData,nDataLen,tIV,strAppPass.c_str());
     return String((char*)tData).substring(0, nDataLen);
 }
 
