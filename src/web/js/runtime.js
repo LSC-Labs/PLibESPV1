506e1917f0aee2473857f7d5475e244c2ca93d83
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2026-04-09 11:21:09 +0200

    Translator upate

--- src/web/js/runtime.js
+++ src/web/js/runtime.js
@@ -1569,28 +1569,32 @@ class CTranslator {
             let tKeys = i18nKey.split(".");
             oData = tKeys.reduce((obj, i) => obj[i], oLangDef);
         } catch  {}
-        // If result is an array, it is an array of strings...
-        // map all strings
-        // in this array into one result.
-        if(Array.isArray(oData)) {
-            let strResult = "";
-            for(let strLine of oData) {
-                if(Utils.isString(strLine)) strResult += strLine;
-            }
-            oData = strResult;
-        }
         return(oData);
     }
 
 
-    getAsString(oData, strDefault) {
-        let strResult = "";
+    /**
+     * Resolve the data to a string.
+     * If it is an array, the values will be joined,
+     * If it is already a string, it will stay in place.
+     * In all other cases, the result will be "" or the strDefault.
+     * If a VarTable is as parameter, the final result will be substituted
+     * @param {*} oData 
+     * @param {CVarTable|undefined} oVarTable 
+     * @param {string|undefined} strDefault 
+     * @returns strDefault if no data, otherwise the string result of the data, with substituted vars (if var table in place).
+     */
+    _resolveToString(oData, oVarTable, strDefault) {
+        let strResult = Utils.isString(strDefault) ? strDefault : "";
         if(oData) {
             if(Array.isArray(oData)) {
                 for(let strLine of oData) strResult += strLine;
-            } else {
+            } else if(Utils.isString(oData)) {
                 strResult = oData;
             }
+        }
+        if(Utils.isString(strResult) && Utils.isInstanceOf(oVarTable,"CVarTable")) {
+            strResult = oVarTable.subst(strResult);
         }
         return(strResult ?? strDefault);
     }
@@ -1641,12 +1645,17 @@ class CTranslator {
      */
     _setElementProps(oElement,oData, oVarTable) {
         let bSubst = oVarTable && oVarTable.subst;
+        // If it is an array - resolve the array to a string first.
         if(Array.isArray(oData)) {
-            oData = this.getAsString(oData)
+            oData = this._resolveToString(oData)
         }
+        // If data is a string, set the innerHTML of the element with the substituted string.
         if(typeof oData === 'string') {
             oElement.innerHTML = bSubst ? oVarTable.subst(oData) : oData;
         }
+        // complex structure with proerties.
+        // set all @... properties as attributes,
+        // and all text/html properties as innerText/innerHTML
         else if(typeof oData === 'object') {
             // If object, all "@..." elements are attribute information
             // The the target has this attribute, it will be replaced...
@@ -1656,9 +1665,11 @@ class CTranslator {
                     if(this._bForceAttributes || oElement.hasAttribute(strAttr)) {
                         oElement.setAttribute(strAttr,bSubst ? oVarTable.subst(oData[strKey]) : oData[strKey]);
                     }
+                    
                 } else {
-                    // set element by name
-                    let strVal = bSubst ? oVarTable.subst(oData[strKey]) : oData[strKey]
+                    // set element html/text with property value
+                    // muast be a string or an array of strings, otherwise it will be empty.
+                    let strVal = this._resolveToString(oData[strKey],oVarTable);
                     switch(strKey) {
                         case "text": oElement.innerText = strVal; break;
                         case "html": oElement.innerHTML = strVal; break;
@@ -1670,11 +1681,25 @@ class CTranslator {
         return(oElement);
     }
 
+    /**
+     * translates async a i18n key into a string
+     * Will not operate on key:target syntax, only native keys are supported.
+     * The result will be substituted against the var table, if in place.
+     * @param {string} strI18n The i18n key to be translated
+     * @param {string} strLanguage The language to be used for translation
+     * @param {CVarTable} oVars The var table for substitution of the result string.
+    * @returns the translated and substituted string.
+     */
     async translateI18n(strI18n,strLanguage,oVars) {
         let strResult = "";
         await this.getKeyData(strI18n,strLanguage)
-            .then(str => {
-                strResult = oVars ? oVars.subst(str) : str;
+            .then(oData => {
+                if(Array.isArray(oData)) {
+                    let s = "";
+                    for(let strLine of oData) s += strLine;
+                    oData = s;
+                }
+                strResult = Utils.isInstanceOf(oVars,CVarTable) ? oVars.subst(oData) : oData;
             })
         return(strResult);
     }
@@ -1682,10 +1707,10 @@ class CTranslator {
     /**
      * Translate an (html) element into the requested language
      * and substitute it against the var table
-     * @param {HTMLElement|CElement} oElement         The element to be translated
-     * @param {string|undefined} strLanguage Default is the userlanguage code
-     * @param {CVarTable} oVars              The vars of the application
-     * @param {boolean} bWithChilds false == only the element, true == also the child elements
+     * @param {HTMLElement|CElement} oElement The element to be translated
+     * @param {string|undefined} strLanguage  Default is the userlanguage code
+     * @param {CVarTable} oVars               The vars of the application
+     * @param {boolean} bWithChilds false == only the element, true == also the child elements (default)
      */
     async translate(oElement, strLanguage, oVars, bWithChilds = true) {
         if(!strLanguage) strLanguage = this.getUserLanguage();
@@ -1695,6 +1720,8 @@ class CTranslator {
             oElement = oHTMLElement;
             let strI18n = oElement.dataset ? oElement.dataset.i18n : undefined;
             if(strI18n && typeof strLanguage === 'string') {
+                // Check if more then one entry is in the i18n definition
+                // - Split up to see if a key:value definition is in place
                 for(let strSelect of strI18n.split("|")) {
                     let strKey = strSelect;
                     let strTarget;
@@ -1704,13 +1731,17 @@ class CTranslator {
                         strTarget = strSelect.substring(0,nPos);
                     }
                     
+                    // get the i18n key data (async)
                     this.getKeyData(strKey,strLanguage)
                         .then(oData => {
+                            // Syntax 1: "target:key|..." 
                             if(strTarget) {
+                                // If an attribute is defined as target, set the attribute 1:1 with the result.
                                 if(strTarget.startsWith("@")) oElement.setAttribute(strTarget.substring(1),oData);
+                                // Otherwise, set the inner html of the target element 
                                 else {
                                     let oTargetElement = oElement.querySelector(strTarget);
-                                    if(oTargetElement) oTargetElement.innerHTML = this.getAsString(oData);
+                                    if(oTargetElement) oTargetElement.innerHTML = this._resolveToString(oData,oVars);
                                 }
                             }
                             let oResult = strTarget ? oElement : this._setElementProps(oElement,oData,oVars);
